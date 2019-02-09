aef600fba05c4150e6a879717b7cf3b0154d24d4
Marcus9191 <47478477+Marcus9191@users.noreply.github.com>
2019-02-09 19:53:36 +0100

    Addnodes

---
+++ addnodes.bash
@@ -0,0 +1,17 @@
+FOLDER="3dcoin"
+CONFIG_FOLDER="$HOME/.$FOLDER"
+
+echo
+"addnode=206.189.72.203
+addnode=206.189.41.191
+addnode=165.227.197.115
+addnode=167.99.87.86
+addnode=159.65.201.222
+addnode=159.65.148.226
+addnode=165.227.38.214
+addnode=159.65.167.79
+addnode=159.65.90.101
+addnode=128.199.218.139
+addnode=174.138.3.33
+addnode=159.203.167.75
+addnode=138.68.102.67" >> $HOME/.$FOLDER/$COIN_NAME.conf
